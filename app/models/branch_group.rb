66b38a26460614e59a97ce6331c12ec9b7696ba2
Adones <pitogo.adones@gmail.com>
2014-07-10 17:10:46 +0800

    user-branch_group association

--- app/models/branch_group.rb
+++ app/models/branch_group.rb
@@ -1,7 +1,7 @@
 class BranchGroup < ActiveRecord::Base
 
   # RELATIONSHIPS
-  has_and_belongs_to_many :admins,
+  has_and_belongs_to_many :admins, -> { where role: "resto_admin" },
                           class_name: "User",
                           association_foreign_key: "user_id",
                           join_table: "admins_branch_groups"
