4f1b294aac46e896f0334e6bcc4badde96cae0b3
Adones <pitogo.adones@gmail.com>
2014-07-10 17:17:47 +0800

    restaurant-cuisine relationship

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,2 +1,4 @@
 class Cuisine < ActiveRecord::Base
+  # RELATIONSHIPS
+  has_and_belongs_to_many :restaurants
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ActiveRecord::Base
+  # RELATIONSHIPS
+  has_and_belongs_to_many :cuisines
 end
