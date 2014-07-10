bcac97c6c89046bcb48629c6851bb38fb8c56765
Adones <pitogo.adones@gmail.com>
2014-07-10 22:11:32 +0800

    user_activity assoc to self

--- app/models/user_activity.rb
+++ app/models/user_activity.rb
@@ -1,4 +1,6 @@
 class UserActivity < ActiveRecord::Base
   # relations
   belongs_to :user
+  belongs_to :parent, class_name: "UserActivity", foreign_key: "parent_id"
+  has_one :child, class_name: "UserActivity", foreign_key: "parent_id"
 end
