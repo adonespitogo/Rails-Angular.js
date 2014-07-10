d3714d7e2306f96d24806bba7e1ca64917d76b9f
Adones <pitogo.adones@gmail.com>
2014-07-10 16:45:54 +0800

    user-branch_group association

--- app/models/branch_group.rb
+++ app/models/branch_group.rb
@@ -1,2 +1,8 @@
 class BranchGroup < ActiveRecord::Base
+
+  # RELATIONSHIPS
+  has_and_belongs_to_many :admins,
+                          class_name: "User",
+                          association_foreign_key: "user_id",
+                          join_table: "admins_branch_groups"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  # RELATIONSHIPS
+  has_and_belongs_to_many :branch_groups,
+                          join_table: "admins_branch_groups"
 end
