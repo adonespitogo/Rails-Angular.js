7c06cd919d62fb53063a3f5321a2afb62c0dfb60
Adones <pitogo.adones@gmail.com>
2014-07-10 21:23:32 +0800

    menu_category:menu_item assoc

--- app/models/menu_category.rb
+++ app/models/menu_category.rb
@@ -2,4 +2,8 @@ class MenuCategory < ActiveRecord::Base
   # relationships
   has_and_belongs_to_many :branch_groups
   has_and_belongs_to_many :branches
+
+  has_and_belongs_to_many :menu_items,
+                            class_name: "MenuItem",
+                            join_table: "menu_categories_menu_items"
 end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,2 +1,6 @@
 class MenuItem < ActiveRecord::Base
+  # relationships
+  has_and_belongs_to_many :menu_categories,
+                            class_name: "MenuCategory",
+                            join_table: "menu_categories_menu_items"
 end
