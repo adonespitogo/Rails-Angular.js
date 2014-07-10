562b9a479aadbf49c8fd961f7028e4595d880ac3
Adones <pitogo.adones@gmail.com>
2014-07-10 16:19:05 +0800

    added user blueprint for :resto_admin

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,9 +1,14 @@
 require 'machinist/active_record'
+require 'faker'
 
-# Add your blueprints here.
-#
-# e.g.
-#   Post.blueprint do
-#     title { "Post #{sn}" }
-#     body  { "Lorem ipsum..." }
-#   end
+User.blueprint(:resto_admin) do
+  role      { "resto_admin" }
+  email     { Faker::Internet.safe_email }
+  username  { "resto_admin#{sn}" }
+  password  { "12345678" }
+  encrypted_password { "$2a$10$qQ9e9c9rWsQ0RDBq1F2Ske77reWEZJozr7rOPNNoMauVDN2M/HDaW" }
+  lastname  { Faker::Name.last_name }
+  lastname  { Faker::Name.last_name }
+  phone_number { Faker::PhoneNumber.cell_phone }
+  address { "#{Faker::Address.street_address}, #{Faker::Address.state}, #{Faker::Address.country}" }
+end
