518de72682f2a3853888987f404bb4df677d71a9
Adones <pitogo.adones@gmail.com>
2014-07-28 01:18:47 +0800

    can create category

---
+++ app/controllers/resto_admin/menu_categories_controller.rb
@@ -0,0 +1,26 @@
+class RestoAdmin::MenuCategoriesController < RestoAdmin::BaseController
+
+  protect_from_forgery with: :null_session
+  skip_before_filter :verify_authenticity_token
+
+  respond_to :json
+  layout false
+
+  def index
+    @categories = @branch_group.branches_categories
+  end
+
+  def create
+    @category = MenuCategory.new(category_params)
+    @category.save
+    @branch_group.branches.each do |branch|
+      BranchMenuCategory.create(branch_id: branch.id, menu_category_id: @category.id)
+    end
+    render :show
+  end
+
+  private
+    def category_params
+      params.permit(:name, :slug, :meta_keywords, :meta_description)
+    end
+end
